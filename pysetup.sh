e74259a7c14eb94239e659faae3c96d7a6b8f8ea
Markus Gschwendt <markus@gschwendt.at>
2018-05-14 16:42:57 +0200

    templates for nginx and uwsgi

--- pysetup.sh
+++ pysetup.sh
@@ -5,3 +5,13 @@ pip3 install -r requirements.txt
 ln -s venv/bin/activate
 echo "export FLASK_CONFIG=development; export FLASK_APP=run.py; export PYTHONPATH=${PYTHONPATH}:$(dirname ${0})" >> activate
 
+sed "s/###dir###/$(dirname ${0})/" doc/nginx/veximpy.template > doc/nginx/veximpy
+sed "s/###dir###/$(dirname ${0})/" doc/uwsgi/veximpy.ini.template > doc/uwsgi/veximpy.ini
+
+if [[ ! -z ${1} ]]; then
+    sed "s/###servername###/${1}/" doc/nginx/veximpy.template > doc/nginx/veximpy
+fi
+
+echo "you will find sample config files for nginx and uwsgi in doc/{nginx|uwsgi} directories"
+echo "make sure to use the correct path for certificates and the correct servername"
+echo "you should be able to activate the python environment with \`. activate\`"
